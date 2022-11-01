8d00ab5d3497393a1028d3b02baa7c3a7c8151dc
StaceyJo <stacey.l.joseph@gmail.com>
2022-11-01 16:08:06 -0400

    populated game board so snake can move

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Creating a gameboard object I can manipulate
 // so I don't have to create 144 divs within the gameboard
 
-// Why is this not working?!
-const board = document.querySelector("gameboard");
-board.innerHTML = "div></div>".repeat(144);
+
+// This wasn't working at first. 
+//Make sure you add width and height if you do this again
+const width = 12;
+const height = 12;
+const board = document.querySelector("#gameboard");
+board.innerHTML = "<div></div>".repeat(144);
